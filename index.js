3cc34f61b44293aa639ae5bdcf07e668a9e045ff
Culla95 <63858355+Culla95@users.noreply.github.com>
2021-10-21 19:48:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1047,7 +1047,7 @@ async function transcribe_witai(buffer) {
         const output = await extractSpeechIntent(WITAPIKEY, stream, contenttype)
         var temp_output = output.split(",");
         var final_output = temp_output[temp_output.length-2]
-        final_output = final_output.substr(12,(final_output.length-1));
+        final_output = final_output.substr(12,(final_output.length-2));
         witAI_lastcallTS = Math.floor(new Date());
         console.log(output)
         stream.destroy()
