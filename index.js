ed76f1a65124b8db4de7fbca15a62990a3c1e20c
Culla95 <63858355+Culla95@users.noreply.github.com>
2021-10-21 16:12:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -931,7 +931,8 @@ async function transcribe_witai(buffer) {
         const contenttype = "audio/raw;encoding=signed-integer;bits=16;rate=48k;endian=little"
         const output = await extractSpeechIntent(WITAPIKEY, stream, contenttype)
         witAI_lastcallTS = Math.floor(new Date());
-        var pruebaaaa = output[((output.length)-1)];
+        //var pruebaaaa = output[((output.length)-1)];
+        var pruebaaaa = output[text];
         console.log("transcribewitaioutput: " + output)
         console.log("pruebaaaa: " + pruebaaaa)
         stream.destroy()
