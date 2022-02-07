9c3795b749bc7d19f14392fd2dc80c04ddbc875d
Culla95 <63858355+Culla95@users.noreply.github.com>
2022-02-07 15:58:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -279,7 +279,8 @@ discordClient.on('message', async (msg) => {
         else if (msg.content.trim().toLowerCase() == _CMD_LIMPIEZA) {
             
             const channel = msg.channel;
-            channel.fetchMessages().then(messages => {
+            const messageManager = channel.messages;
+            messageManager.fetch({ limit: 100 }).then(messages => {
                 var msgToRemove1 = messages.filter(m => m.content.startsWith(PREFIX))
                 var msgToRemove2 = messages.filter(m => m.author.id == 523228821533753354)
                 channel.bulkDelete(msgToRemove1);
